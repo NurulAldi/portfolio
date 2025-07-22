c73da097a47e04dbb1f10d19281782757f3119fa
aldi <aldiprm48@gmail.com>
2025-07-22 18:27:13 +0700

    initial contact form done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,128 @@
 import Image from "next/image";
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="px-20 py-12 flex flex-col">
+      <section className="min-h-screen flex flex-col justify-between">
+        {/* Header */}
+        <div className="flex justify-between items-center">
+          <Image 
+            src={"/portfolio-logo.svg"}
+            alt=""
+            width={40}
+            height={40}
+            className="rounded"
+          />
+          <div className="flex gap-16 text-2xl">
+            <a href="#"><FaGithub className="text-4xl " /></a>
+            <a href="#"><FaLinkedinIn className="text-4xl" /></a>
+            <a href="#"><FaInstagram className="text-4xl" /></a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        {/* Content */}
+        <div className="flex flex-col lg:flex-row justify-between items-center flex-grow mt-20">
+          {/* Left Text */}
+          <div className="max-w-xl text-center lg:text-left">
+            <p className="text-gray-600 text-2xl mb-2">Hi, I’m <span className="text-black font-semibold underline">Aldi</span></p>
+            <h1 className="text-4xl lg:text-5xl font-bold mb-4">Web Developer</h1>
+            <p className="text-gray-600 mb-6">
+              from Indonesia, passionate about turning ideas into clean,
+              functional, and scalable websites.
+            </p>
+            <div className="flex gap-4 justify-center lg:justify-start">
+              <button className="bg-black text-white px-4 py-2 rounded transition duration-300 hover:-translate-y-1 hover:shadow-xl">Contact Me</button>
+              <button className="border border-black px-4 py-2 rounded transition duration-300 hover:-translate-y-1 hover:shadow-xl">My Projects</button>
+            </div>
+          </div>
+
+          {/* Right Picture */}
+          <Image 
+            src={"/dummy-pict.svg"}
+            alt=""
+            width={300}
+            height={400}
+            className="rounded"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </section>
+
+      {/* Project Section */}
+      <section className="mt-36 min-h-screen">
+        <p className="text-gray-600 mb-8">
+          I specialize in building sites using Next.js and Tailwind CSS,<br />
+          Here’s some of the projects I’ve built :
+        </p>
+
+        <h2 className="text-2xl font-bold mb-12">My Projects</h2>
+
+        <div className="flex flex-col lg:flex-row items-start gap 10 sticky top-1/2 max-h-min">
+          <div className="flex items-start gap-4 flex-1 h-full">
+            <div className="text-9xl font-bold text-gray-500">1</div>
+            <div className="flex flex-col items-start justify-center h-full">
+              <h3 className="font-semibold">Nagari Limokoto</h3>
+              <p className="text-sm text-gray-600 max-w-sm">
+                Helping local communities go digital, this website presents the
+                village’s profile and enables residents to submit administrative
+                forms easily and efficiently.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex gap-2">
+            <Image 
+              src={"/preview-web-desktop.svg"}
+              alt="desktop preview"
+              width={312}
+              height={192}
+              className="rounded"
+            />
+            <Image 
+              src={"/preview-web-mobile.svg"}
+              alt="desktop preview"
+              width={100}
+              height={72}
+              className="rounded"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section className="mt-32 mb-20 min-h-screen">
+        <p className="text-center text-gray-600 mb-4 uppercase tracking-wide">Need a Developer?</p>
+        <div className="max-w-md mx-auto border p-8 bg-white">
+          <h2 className="text-center text-xl font-bold mb-2">Contact Me</h2>
+          <p className="text-center text-sm text-gray-600 mb-6">Let's work together!<br />Fill out this short form and I’ll get back to you as soon as possible.</p>
+
+          <form className="flex flex-col gap-4">
+            <div>
+              <label className="block mb-1 text-sm font-semibold">Name</label>
+              <input type="text" className="w-full border p-2" />
+            </div>
+            <div>
+              <label className="block mb-1 text-sm font-semibold">Email</label>
+              <input type="text" className="w-full border p-2" />
+            </div>
+            <div>
+              <label className="block mb-1 text-sm font-semibold">Message</label>
+              <textarea rows={4} className="w-full border p-2"></textarea>
+            </div>
+            <button type="submit" className="bg-black text-white py-2 rounded shadow transition-all duration-300 ease-in-out
+             hover:bg-white hover:text-black hover:border hover:border-black
+             hover:shadow-xl">Submit</button>
+          </form>
+
+          <p className="text-center text-sm text-gray-600 mt-4">
+            Or email me directly at <a href="mailto:aldi.dev@email.com" className="underline">aldi.dev@email.com</a>
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
